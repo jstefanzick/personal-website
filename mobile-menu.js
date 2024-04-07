dc9257e6c73ef2ac25a4c129c6c7d4d609806276
jstefanzick <stefanzi@seattleu.edu>
2024-04-07 15:55:47 -0700

    Mobile hamburger menu opens up

---
+++ mobile-menu.js
@@ -0,0 +1,9 @@
+const hamMenu = document.querySelector('.ham-menu');
+console.log(hamMenu);
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+})
