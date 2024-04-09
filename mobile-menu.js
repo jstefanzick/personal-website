2ed54d476e475f398a21c5f65f5e91a3a0c9a43a
jstefanzick <stefanzi@seattleu.edu>
2024-04-08 18:49:42 -0700

    Can close menu by clicking elsewhere'

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,16 +1,32 @@
 const hamMenu = document.querySelector('.ham-menu');
-
 const offScreenMenu = document.querySelector('.off-screen-menu');
+const footerClick = document.querySelector('footer');
 
 hamMenu.addEventListener('click', () => {
-  hamMenu.classList.toggle('active');
-  offScreenMenu.classList.toggle('active');
-})
+  toggleActive();
+});
 
 window.onresize = function () {
   var w = window.outerWidth;
   if (w > 833 && hamMenu.classList.contains('active')){
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
+    toggleActive();
   }
-}
+};
+
+window.onclick = function(e){
+  if(!e.target.classList.contains("ham-list") &&
+    !e.target.classList.contains("off-screen-menu") &&
+    !e.target.classList.contains("ham-span") &&
+    !e.target.classList.contains("ham-menu") &&
+    hamMenu.classList.contains('active')){
+    toggleActive();
+  }
+};
+
+toggleActive = () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+  footerClick.classList.toggle('active');
+};
+
+
